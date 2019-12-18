afe15808df152c3e339df3401f750d92de690d4a
Jostein Brændshøi <jostein.brandshoi@gmail.com>
2019-12-18 14:10:43 +0100

    refactored into Section class and renamed files

--- sections/section.py
+++ sections/section.py
@@ -1,79 +1,44 @@
 
-# Functions for computing uniformly spaced (in meters) lat,lon coordinates
-# between defined edge coordinates
+# Functions for reading writing lat,lon sections from file specifying egde
+# coordinates to file specifying the entire lat,lon coordinates a long a line
+# between the edge points to be used by cdo
 
-import math
+import numpy as np
+import netCDF4
+import section
+import pandas
 
-def get_path_length(lat1,lng1,lat2,lng2):
-    """calculates the distance between two lat, long coordinate pairs"""
-    R = 6371000 # radius of earth in m
-    lat1rads = math.radians(lat1)
-    lat2rads = math.radians(lat2)
-    deltaLat = math.radians((lat2-lat1))
-    deltaLng = math.radians((lng2-lng1))
-    a = math.sin(deltaLat/2) * math.sin(deltaLat/2) + math.cos(lat1rads) * math.cos(lat2rads) * math.sin(deltaLng/2) * math.sin(deltaLng/2)
-    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
-    d = R * c
-    return d
+def read_section(filename):
+    """Function that reads section edge coordinatesfrom file."""
+    return pandas.read_csv(filename, index_col="coordinates")
 
-def get_destination_latlon(lat,lng,azimuth,distance):
-    """returns the lat an long of destination point
-    given the start lat, long, aziuth, and distance"""
-    R = 6378.1 #Radius of the Earth in km
-    brng = math.radians(azimuth) #Bearing is degrees converted to radians.
-    d = distance/1000 #Distance m converted to km
-    lat1 = math.radians(lat) #Current dd lat point converted to radians
-    lon1 = math.radians(lng) #Current dd long point converted to radians
-    lat2 = math.asin(math.sin(lat1) * math.cos(d/R) + math.cos(lat1)* math.sin(d/R)* math.cos(brng))
-    lon2 = lon1 + math.atan2(math.sin(brng) * math.sin(d/R)* math.cos(lat1), math.cos(d/R)- math.sin(lat1)* math.sin(lat2))
-    #convert back to degrees
-    lat2 = math.degrees(lat2)
-    lon2 = math.degrees(lon2)
-    return[lat2, lon2]
+def write_gridfile(filename, coors):
+    """Function that writes a gridfile for cdo specfying a section,
+    i.e. giving lat,lon coordinates along the section."""
+    lats = ",".join([str(l[0]) for l in coors])
+    lons = ",".join([str(l[1]) for l in coors])
 
-def calculate_bearing(lat1,lng1,lat2,lng2):
-    """calculates the azimuth in degrees from start point to end point"""
-    startLat = math.radians(lat1)
-    startLong = math.radians(lng1)
-    endLat = math.radians(lat2)
-    endLong = math.radians(lng2)
-    dLong = endLong - startLong
-    dPhi = math.log(math.tan(endLat/2.0+math.pi/4.0)/math.tan(startLat/2.0+math.pi/4.0))
-    if abs(dLong) > math.pi:
-         if dLong > 0.0:
-             dLong = -(2.0 * math.pi - dLong)
-         else:
-             dLong = (2.0 * math.pi + dLong)
-    bearing = (math.degrees(math.atan2(dLong, dPhi)) + 360.0) % 360.0;
-    return bearing
-
-def get_coordinates(interval,azimuth,lat1,lng1,lat2,lng2):
-    """returns every coordinate pair inbetween two coordinate
-    pairs given the desired interval"""
-
-    d = get_path_length(lat1,lng1,lat2,lng2)
-    remainder, dist = math.modf((d / interval))
-    counter = float(interval)
-    coords = []
-    coords.append([lat1,lng1])
-    for distance in xrange(0,int(dist)):
-        coord = get_destination_latlon(lat1,lng1,azimuth,counter)
-        counter = counter + float(interval)
-        coords.append(coord)
-    coords.append([lat2,lng2])
-    return coords
+    with open(filename, "w") as f:
+        f.write("gridtype = curvilinear\n")
+        f.write("gridsize = {}\n".format(len(lats)))
+        f.write("xsize = {}\n".format(len(lats)))
+        f.write("ysize = 1\n")
+        f.write("\n")
+        f.write("# Longitudes\n")
+        print(lons)
+        f.write(lons)
+        f.write("\n\n")
+        f.write("# Latitudes\n")
+        f.write(lats)
+        f.write("\n")
 
 if __name__ == "__main__":
-    #point interval in meters
-    interval = 50000.0
-    #direction of line in degrees
-    #start point
-    lat1 = 77
-    lng1 = 20
-    #end point
-    lat2 = 70
-    lng2 = 20
-    azimuth = calculate_bearing(lat1,lng1,lat2,lng2)
-    print azimuth
-    coords = get_coordinates(interval,azimuth,lat1,lng1,lat2,lng2)
-    print coords
+    filename = "cdo_gridfile.txt"
+    interval = 10000.0
+    lat0 = 77
+    lon0 = 20
+    lat1 = 70
+    lon1 = 20
+    azimuth = section.calculate_bearing(lat0,lon0,lat1,lon1)
+    coors = section.get_coordinates(interval,azimuth,lat0,lon0,lat1,lon1)
+    write_gridfile(filename, coors)
