4fc1b670c348a426dda850e202cd49ffdcee4cc6
johannesro <johannesro@met.no>
2020-02-14 14:29:52 +0000

    rotate velocities ready for testing

--- transport/rotate_velocities.py
+++ transport/rotate_velocities.py
@@ -1,13 +1,10 @@
+#!/use/bin/env python
 import scipy as sp
 import numpy as np
 import scipy.ndimage as nd
 from netCDF4 import Dataset
 import sys
 #from get_latlon import *
-
-sectionfilename = sys.argv[1]
-#alphafilename = sys.argv[2]
-
 
 def rotate_vectorfield(U,V,alpha):
     '''rotate wind vectors clockwise. alpha may be a scalar or an array
@@ -18,36 +15,60 @@ def rotate_vectorfield(U,V,alpha):
     R = sp.array([[sp.cos(alpha), -sp.sin(alpha)], [sp.sin(alpha), sp.cos(alpha)] ])
     shpe = U.shape
     origwind = sp.array((U.flatten(), V.flatten()))
-    #if len(R.shape)==2:
-    #    rotwind = np.dot(R, origwind) # for constant rotation angle
-    #else:
-    # for rotation angle given as array with same dimensions as U and V:
-    # k-loop with rotwind(k) = dot(R(i,j,k), origwind(j,k)) (einstein summation indices)
-    rotwind = sp.einsum("ijk,ik -> jk", R, origwind)  # einstein summation indices
-
+    if len(R.shape)==2:
+        rotwind = np.dot(R, origwind) # for constant rotation angle
+    else:
+        # for rotation angle given as array with same dimensions as U and V:
+        # k-loop with rotwind(k) = dot(R(i,j,k), origwind(j,k)) (einstein summation indices)
+        # i: points, j: y vector, k: x vector
+        rotwind = sp.einsum("ijk,ik -> jk", R, origwind)  # einstein summation indices
     Urot ,Vrot = rotwind[0,:], rotwind[1,:]
     Urot = Urot.reshape(shpe)
     Vrot = Vrot.reshape(shpe)
     return Urot, Vrot
 
-def north_direction(lat):
-    '''get the north direction relative to image positive y coordinate'''
-    dlatdx = nd.filters.sobel(lat,axis=1,mode='constant',cval=sp.nan) #gradient in x-direction
-    dlatdy = nd.filters.sobel(lat,axis=0,mode='constant',cval=sp.nan)
-    ydir = lat[-1,0] -lat[0,0] # check if latitude is ascending or descending in y axis
-    # same step might have to be done with x direction.
-    return sp.arctan2(dlatdx,dlatdy*sp.sign(ydir) )*180/sp.pi
+def append_section_referenced_velocities(sec_data_file, sec_mod_grid):
+    '''rotate u,v velocities in a section data file from model grid to section grid
+       and append rotated velocities to section data file
+
+       new u velocities are along section and new
+           v velocities are across section
+
+       append_section_referenced_velocities(section_data_file, sec_mod_file) '''
+
+    sdf = Dataset(sec_data_file, 'r+')
+    u = sdf.variables['u'] 
+    v = sdf.variables['v']
+
+    smg = Dataset(sec_mod_file, 'r')
+    # alpha is the angel from section x direction to model x direction, 
+    # section x is defined as along-section and section y is perpendicular to the section
+    alpha = smg.variables['alpha']
+
+    u_sec, v_sec = rotate_vectorfield(u[:], v[:], -alpha[:]) # rotate from model x to section x
+
+    u_sec_nc = sdf.createVariable('u_sec', u.datatype, u.dimensions)
+    u_sec_nc.setncatts({k: u.getncattr(k) for k in u.ncattrs()})
+    u_sec_nc.setncatts({'long_name' : 'along_section_velocity'})
+    u_sec_nc.setncatts({'standard_name' : 'along_section_velocity'})
+
+    v_sec_nc = sdf.createVariable('v_sec', v.datatype, v.dimensions)
+    v_sec_nc.setncatts({k: v.getncattr(k) for k in v.ncattrs()})
+    v_sec_nc.setncatts({'long_name' : 'cross_section_velocity'})
+    v_sec_nc.setncatts({'standard_name' : 'cross_section_velocity'})
+
+    sdf.close()
+    smg.close()
+
+if __name__ == "__main__":
+    sec_data_file = sys.argv[1]
+    sec_mod_grid = sys.argv[2]
+    append_section_referenced_velocities(sec_data_file, sec_mod_grid)
 
 
-sf = Dataset(sectionfilename)
-u = sf.variables['u'][:]
-v = sf.variables['v'][:]
-alpha = sf.variables['alpha'][:]
-
-#lat_grid, lon_grid = get_latlon_from_topaz()
-
-sec_northdir  = north_direction(sec_lat.T)
 
 
 
-#alpha = sec_northdir - grid_northdir
+
+
+
